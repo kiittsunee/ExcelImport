289e4245313f0b9bae337a0458f8457dff6d2266
Anna <annaocirova5@gmail.com>
2023-07-10 13:14:43 +0800

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelImport
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
